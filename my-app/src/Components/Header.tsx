3e05bb5d14153b70bd6cdad8a933a2e688df3cf4
landlala <land_lala@naver.com>
2023-05-13 01:44:00 +0000

    #9.3

--- my-app/src/Components/Header.tsx
+++ my-app/src/Components/Header.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {useState} from "react";
 import {motion} from "framer-motion";
 import {Link, useMatch} from "react-router-dom";
 
@@ -52,12 +53,15 @@ const Item = styled.li`
 
 const Search = styled.span`
   color: white;
+  display: flex;
+  align-items: center;
+  position: relative;
   svg {
     height: 25px;
   }
 `;
 
-const Circle = styled.span`
+const Circle = styled(motion.span)`
   position: absolute;
   width: 5px;
   height: 5px;
@@ -67,6 +71,12 @@ const Circle = styled.span`
   left: 0;
   right: 0;
   margin: 0 auto;
+`;
+
+const Input = styled(motion.input)`
+  transform-origin: right center;
+  position: absolute;
+  left: -150px;
 `;
 
 const logoVariants = {
@@ -82,9 +92,10 @@ const logoVariants = {
 };
 
 function Header() {
+  const [searchOpen, setSearchOpen] = useState(false);
   const homeMatch = useMatch("/");
   const tvMatch = useMatch("tv");
-  console.log(homeMatch, tvMatch);
+  const toggleSearch = () => setSearchOpen(prev => !prev);
   
   return (
     <Nav>
@@ -103,19 +114,22 @@ function Header() {
         <Items>
           <Link to = "/">
             <Item>
-              Home {homeMatch && <Circle />}
+              Home {homeMatch && <Circle layoutId = "circle" />}
             </Item>
           </Link>
           <Link to = "tv">
             <Item>
-              Tv Shows {tvMatch && <Circle />}
+              Tv Shows {tvMatch && <Circle layoutId = "circle" />}
             </Item>
           </Link>
         </Items>
       </Col>
       <Col>
         <Search>
-          <svg
+          <motion.svg
+            onClick = {toggleSearch}
+            animate = {{x: searchOpen ? -180 : 0}}
+            transition = {{type: "linear"}}
             fill="currentColor"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
@@ -125,7 +139,12 @@ function Header() {
               d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
               clipRule="evenodd"
             />
-          </svg>
+          </motion.svg>
+          <Input
+            animate = {{scaleX: searchOpen ? 1 : 0}} 
+            transition = {{type: "linear"}} 
+            placeholder = "search for movie or tv show" 
+          />
         </Search>
       </Col>
     </Nav>
