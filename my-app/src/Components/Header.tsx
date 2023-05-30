4d9939a4d179023ff39a487151950db7e1ab15b6
landlala <land_lala@naver.com>
2023-05-30 06:16:26 +0000

    #9.14

--- my-app/src/Components/Header.tsx
+++ my-app/src/Components/Header.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import {useState, useEffect} from "react";
 import {motion, useAnimation, useScroll, useMotionValueEvent} from "framer-motion";
-import {Link, useMatch} from "react-router-dom";
+import {Link, useMatch, useNavigate} from "react-router-dom";
+import {useForm} from "react-hook-form";
 
 const Nav = styled(motion.nav)`
   display: flex;
@@ -50,7 +51,7 @@ const Item = styled.li`
   }
 `;
 
-const Search = styled.span`
+const Search = styled.form`
   color: white;
   display: flex;
   align-items: center;
@@ -106,6 +107,10 @@ const navVariants = {
   },
 };
 
+interface IForm {
+  keyword: string;
+}
+
 function Header() {
   const [searchOpen, setSearchOpen] = useState(false);
   const homeMatch = useMatch("/");
@@ -132,7 +137,12 @@ function Header() {
       }
     })
   }, [scrollY, navAnimation]);
-  
+  const navigate = useNavigate();
+  const {register, handleSubmit} = useForm<IForm>();
+  const onValid = (data: IForm) => {
+    console.log(data);
+    navigate(`/search?keyword=${data.keyword}`);
+  };
   return (
     <Nav
       variants = {navVariants}
@@ -165,7 +175,7 @@ function Header() {
         </Items>
       </Col>
       <Col>
-        <Search>
+        <Search onSubmit = {handleSubmit(onValid)}>
           <motion.svg
             onClick = {toggleSearch}
             animate = {{x: searchOpen ? -185 : 0}}
@@ -181,6 +191,7 @@ function Header() {
             />
           </motion.svg>
           <Input
+            {...register("keyword", {required: true, minLength: 2})}
             animate = {inputAnimation}
             initial = {{scaleX: 0}}
             transition = {{type: "linear"}} 
